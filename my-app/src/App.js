427b1f684ff86458ffde20662a1517b619dc253b
Michael Guillemette <mike.guillemette@gmail.com>
2019-06-11 20:37:32 -0400

    This is my first commit.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>ProdPerfect Dashboard App</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Michael Guillemette
         </a>
       </header>
     </div>
