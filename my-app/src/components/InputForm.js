753ead20d736d19a8ac934ee067d9b7513c08f97
Michael Guillemette <mike.guillemette@gmail.com>
2019-07-03 15:44:18 -0400

    Multiple fixes to text formatting. Fixes to CSS sizing, responsiveness.

--- my-app/src/components/InputForm.js
+++ my-app/src/components/InputForm.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 
-class InputForm extends React.Component {
+class InputForm extends Component {
     state = {
-      calculatedTotal: 0,
-      engineers: 5,
-      salary: 55,
+      engineers: 10,
+      salary: 75,
       qa: 3
     }
 
@@ -25,52 +24,82 @@ class InputForm extends React.Component {
       }
 
       console.log(`Devs: ${this.state.engineers}, QA: ${this.state.qa}, Salary: ${this.state.salary}` )
-      let qaPerDev = this.state.engineers / this.state.qa
-      this.setState({
-        calculatedTotal:  this.state.engineers * (qaPerDev * (((this.state.salary * 10) * (this.state.engineers/3)) / 12))
-      })
-      // event.preventDefault();
+      event.preventDefault();
     }
 
     handleReset = () => {
-      console.log('Data(state) has been reset')
       this.setState({
-        calculatedTotal: 0,
-        engineers: 5,
-        salary: 55,
+        engineers: 10,
+        salary: 75,
         qa: 3
       })
     }
   
     render() {
+      let { engineers, salary, qa } = this.state
+      let formatSalary = new Intl.NumberFormat().format(salary * 1000)
+      let qaPerDev = engineers / qa
+      let calculatedTotal = engineers * (qaPerDev * (((salary * 8) * (engineers/3)) / 12))
+      let formatTotal = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(calculatedTotal)
+
       return (
           <div className="inputForm">
-        <form onSubmit={this.handleSubmit} onReset={this.handleReset}>
-        <div className="teamForm">
-          <label>
-            {this.state.engineers} Developers <br/>
-            <input id="dev" type="range" max="30" className="slider" value={this.state.engineers} onChange={this.handleChange} />
-          </label><br/>
-          <label>
-            {this.state.qa} QA Engineers <br />
-            <input id="qa" type="range" max="15" className="slider" value={this.state.qa} onChange={this.handleChange} />
-            {/* <select className="inputField" value={this.state.qa} onChange={this.handleQa}>
-              <option value="3">1 - 3 QA</option>
-              <option value="6">4 - 6 QA</option>
-              <option value="10">7 - 10 QA</option>
-              <option value="15">More than 10 QA</option>
-            </select> */}
-          </label><br/>
-          <label>
-            QA Average Salary: ${this.state.salary * 1000}
-            <input id="salary" type="range" min="45" max="100" step="5"className="slider" value={this.state.salary} onChange={this.handleChange} />
-            {/* <input  className="inputField" type="number" value={this.state.salary} min="45" max="100" step="5" onChange={this.handleSalary}/> */}
-          </label>
-          </div>
-          {/* <input className="submitButton" type="submit" value="Submit" /> */}
-          <input className="resetButton" type="reset" value="Reset" />
-        </form>
-        <h2>Cost of bugs/month: ${this.state.calculatedTotal.toFixed(2)}</h2>
+            <form onSubmit={this.handleSubmit} onReset={this.handleReset}>
+              <div className="teamForm">
+                <label>
+                  { engineers } Developers <br/>
+                  <input 
+                    id="dev" 
+                    type="range" 
+                    max="30" 
+                    className="slider" 
+                    value={ engineers } 
+                    onChange={this.handleChange}
+                    onMouseUp={this.handleChange} 
+                    onTouchMove={this.handleChange}
+                    onTouchEnd={this.handleChange}
+                  />
+                </label><br/>
+                <label>
+                  { qa } QA Engineers <br />
+                  <input 
+                    id="qa" 
+                    type="range" 
+                    max="15" 
+                    className="slider" 
+                    value={ qa } 
+                    onChange={this.handleChange} 
+                    onMouseUp={this.handleChange} 
+                    onTouchMove={this.handleChange}
+                    onTouchEnd={this.handleChange}
+                  />
+                </label><br/>
+                <label className="salary">
+                  QA Average Salary ${formatSalary}
+                  <input 
+                    id="salary" 
+                    type="range" 
+                    min="50" 
+                    max="150" 
+                    step="5"
+                    className="slider" 
+                    value={ salary } 
+                    onChange={this.handleChange} 
+                    onMouseUp={this.handleChange} 
+                    onTouchMove={this.handleChange}
+                    onTouchEnd={this.handleChange}
+                  />
+                </label>
+              </div>
+              <input className="resetButton" type="reset" value="Reset" />
+            </form>
+            <h1 style={{
+              // width: `500px`,
+              fontSize: `calc(10px + 2vmin)`
+            }}
+            >
+            Cost of bugs/month: {formatTotal}
+            </h1>
         </div>
         
       )
