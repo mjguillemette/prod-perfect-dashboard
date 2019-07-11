ad7e37e0a03d4b296cf8a87d18245efb2ab46206
Michael Guillemette <mike.guillemette@gmail.com>
2019-07-11 14:04:02 -0400

    Color tweaks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import './App.css';
-import InputForm from './components/InputForm';
+import React from 'react'
+import './App.css'
+import InputForm from './components/InputForm'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1><span>ProdPerfect</span> Dashboard App</h1>
+        <h1>
+          <span>ProdPerfect</span>Dashboard App
+        </h1>
         <InputForm />
       </header>
     </div>
-  );
+  )
 }
 
-
-export default App;
+export default App
